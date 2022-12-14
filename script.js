94f65592e8b2cfa565ca354154716601c7336d3c
Juan Suñe <111535673+JuanSune@users.noreply.github.com>
2022-12-13 21:56:45 -0300

    up

--- script.js
+++ script.js
@@ -2,22 +2,30 @@ function carregar()
 {
     var d = new Date();
     var dDia = (d.getHours()).toString();
-    var textMSG =window.document.getElementById('msg');
-    var obIMG = document
+    var textMSG = window.document.getElementById('msg');
+    var obIMG = window.document.getElementById('imagem');
+    var colorBack = window.document.body.style;
 
 
     textMSG.innerText = `A hora agora é ${dDia} ou seja, é de`;
+
     if(dDia>4 && dDia<12)
     {
         textMSG.innerText+=` manha`;
-
+        obIMG.src = "manha.jpg";
+        colorBack.backgroundColor  ="#131C45";
     }
     else if(dDia>12 && dDia<19)
     {
         textMSG.innerText+=` tarde`;
+        obIMG.src = "tarde.jpg";
+        colorBack.backgroundColor  ="#131C45";       
     }
     else
     {
         textMSG.innerText+=` noite`;
+        obIMG.src = "noite.jpg";
+        colorBack.backgroundColor  ="#131C45";
     }
+  
 }
